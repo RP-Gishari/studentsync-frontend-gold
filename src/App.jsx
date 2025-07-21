04684aee24b33e466cd3d01ed8bb2714053a3156
Remy <shingiroremy303@gmail.com>
2025-07-21 10:32:58 +0200

    fix: removed some comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StudentList from "./pages/StudentList";
 import StudentDetailPage from "./components/StudentDetailPage";
 import "./App.css";
-// import StudentProfileForm from "./pages/StudentProfileForm";
 
 const App = () => {
   return (
